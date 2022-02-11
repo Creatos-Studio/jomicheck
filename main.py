d8071fb33e935eb05a6f5e837ca073fce8f882aa
Hertzian <tallitosan@gmail.com>
2022-02-10 18:42:29 -0600

    about, all_pass, login, register y se pusieron accesos de login y register al dashboard

---
+++ main.py
@@ -0,0 +1,30 @@
+<<<<<<< Updated upstream
+=======
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+    # return render_template("layout.html")
+
+@app.route('/')
+def login():
+    return render_template('pages/login.html')
+
+@app.route('/register')
+def register():
+    return render_template('pages/register.html')
+
+@app.route('/jomi-passes')
+def all_pass():
+    return render_template('pages/all_pass.html')
+
+@app.route('/about')
+def about():
+    return render_template('pages/about.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+>>>>>>> Stashed changes
